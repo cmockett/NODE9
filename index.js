2de042ad02f65f2171bf92e1d0a51c77c4949284
Student <student@RU-109.local>
2015-10-16 17:07:48 -0600

    3rd

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var bodyParser = require('body-parser')
 var path = require('path')
 var googleTranslate = require('google-translate')('AIzaSyDI9OMHCbsglZ8X805ZVxjEXWazCS3tSvQ');
 var app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
@@ -35,25 +34,51 @@ app.get('/translate', function(req, res){
 	res.send(req.query)
 })
 
-app.post('/getlanguages', function(req, res, next){
+// app.post('/getlanguages', function(req, res, next){
+// 	var wordArray = ["dog", "cat", "house", "table", "water", "tree", "mother", "chair", "mouse", "door"]
+// 	var translatedArray = []
+// 	var quizLanguage = req.body.language
+
+
+var translatedArray = []
+
+app.post('/getlanguages', function(req, res){
 	var wordArray = ["dog", "cat", "house", "table", "water", "tree", "mother", "chair", "mouse", "door"]
-	var translatedArray = []
 	var quizLanguage = req.body.language
-	// for(each in wordArray){
-	// 	googleTranslate.translate(wordArray[each], quizLanguage, function(err, translation){
-	// 		if(err){
-	// 			console.log('nothings happening')
-	// 		}else{
-	// 			translatedArray.push(translation.translatedText)
-	// 		}
-	// 	})
+	for(var i=0;i<wordArray.length; i++){
+		googleTranslate.translate(wordArray[i], quizLanguage, function(err, translation){
+			if(err){
+				console.log('nothings happening')
+			}else {
+				translatedArray.push(translation.translatedText)
+				if(wordArray.length === translatedArray.length){
+							res.send(translatedArray)
+						}
 
-	// }
+			}
+				
+		})
 
+	}
+	translatedArray = []
 })
 
+var checked = 0
 
-
+app.post('/compareanswers', function(req, res){
+	console.log(req.body)
+	// googleTranslate.translate(req.body.word, 'en', function(err, translation){
+			// if(err){
+			// 	console.log('nothings happening')
+			// }else {
+			// 	checked++
+			// 	if(checked === 10){
+			// 		res.send(translation.translatedText)
+			// 	}
+			// }
+				
+		// })
+})
 
 
 
--- public/js/app.js
+++ public/js/app.js
@@ -17,19 +17,51 @@ angular.module('translateApp')
 
 angular.module('translateApp')
 	.controller('quizController', ['$scope', '$http', function($scope, $http){
+		var translatedArray = []
+		var wordArray = ["dog", "cat", "house", "table", "water", "tree", "mother", "chair", "mouse", "door"]
+		$scope.sendTranslation = 
 		$scope.submitLanguage = function(){
+			$scope.sendTranslation = $scope.language
 			$http({
 				method: 'POST',
 				url: '/getlanguages',
+				data : {language : $scope.language}
+			}).then(function(returnData){
+				// console.log(returnData)
+				$scope.translatedArray = returnData.data
+			}, function(error){
+				console.log(error)
+			})
+}
+
+		$scope.submitGuesses = function(key, word, userGuess){
+			console.log($scope.userGuess)
+			$http({
+				method: 'POST',
+				url: '/compareanswers',
+				data : {word: userGuess,
+						language : $scope.sendTranslation}
 			}).then(function(returnData){
 				console.log(returnData)
 			}, function(error){
 				console.log(error)
-			
-			})
+			}
+			)
 
-		}
 
+			// for(var i =0; i<wordArray.length; i++){
+			// $http({
+			// 	method: "POST",
+			// 	url: '/checkanswers',
+			// 	data : {word : $scope.userGuess}
+			// }).then(function(returnData){
+			// 	console.log(returnData.data)
+			// }, function(error){
+			// 	console.log(error)
+			// })
+		// }
+		console.log(key)
+}
 
 
 	}])
